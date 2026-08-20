869120795ae0d19333fbe9acd64a4fcd6c375362
Berat Hazar <berat@sistem.com>
2026-08-20 15:22:01 +0300

    Add production API URL for mobile app

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -2,9 +2,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const API_URL = Platform.select({
-  android: 'http://10.0.2.2:3000/api',
-  ios: 'http://localhost:3000/api',
-  default: 'http://localhost:3000/api'
+  android: __DEV__ ? 'http://10.0.2.2:3000/api' : 'https://boran-mersin.onrender.com/api',
+  ios: __DEV__ ? 'http://localhost:3000/api' : 'https://boran-mersin.onrender.com/api',
+  default: __DEV__ ? 'http://localhost:3000/api' : 'https://boran-mersin.onrender.com/api'
 });
 
 const api = {
